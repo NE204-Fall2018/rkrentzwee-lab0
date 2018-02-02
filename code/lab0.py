78f50d4d7c93c76e70deee31335b4f87be770321
Rebecca Krentz-Wee <rkrentzwee@gmail.com>
2018-02-01 16:04:19 -0800

    Added Eu-152 results

--- code/lab0.py
+++ code/lab0.py
@@ -5,4 +5,5 @@ peaks = cal.loadref('code/sourcedict.txt')
 source_list, spectra = cal.loaddata('data/lab0_spectral_data.txt') 
 m, b = cal.calibrate(['Am241','Cs137'],source_list,spectra,peaks)
 cal.validate('Ba133',m,b,source_list,spectra,peaks) 
+cal.validate('Eu152',m,b,source_list,spectra,peaks) 
 
